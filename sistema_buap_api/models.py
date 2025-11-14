830a2579999f537959cd939019fe1ff19494388b
Hector Mendoza <hector.mendozam@alumno.buap.mx>
2025-11-13 23:17:53 -0600

    Primer commit

---
+++ sistema_buap_api/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authentication import TokenAuthentication
+from django.contrib.auth.models import AbstractUser, User
+from django.conf import settings
+
+class BearerTokenAuthentication(TokenAuthentication):
+    keyword = u"Bearer"
+
+class Administradores(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
+    clave_admin = models.CharField(max_length=255,null=True, blank=True)
+    departamento = models.CharField(max_length=255,null=True, blank=True)
+    creation = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    update = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return "Perfil del admin "+self.first_name+" "+self.last_name
+    
+class Alumnos(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
+    matricula = models.CharField(max_length=255,null=True, blank=True)
+    departamento = models.CharField(max_length=255,null=True, blank=True)
+    creation = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    update = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return "Perfil del alumno "+self.first_name+" "+self.last_name
+
+class Tecnicos(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
+    id_trabajador = models.CharField(max_length=255,null=True, blank=True)
+    departamento = models.CharField(max_length=255,null=True, blank=True)
+    creation = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    update = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return "Perfil del tecnico "+self.first_name+" "+self.last_name
